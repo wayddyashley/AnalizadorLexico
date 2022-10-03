5c96105321613bf3f54cce72c3f9cb401f6e789c
wayddyashley <76131902+wayddyashley@users.noreply.github.com>
2022-10-02 22:51:16 -0400

    Update

---
+++ src/codigo/Tokens.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package codigo;
+
+/**
+ *
+ * @author washl
+ */
+public enum Tokens {
+    Reservadas,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Identificador,
+    Numero,
+    ERROR;
+}
